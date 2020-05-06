ba31afd6833d86531a34df57207a94d8a52ce348
Edwin Torres <codesandtags@gmail.com>
2020-05-05 23:15:15 -0500

    feature: Added optimizations and updated images

---
+++ config/webpack.common.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const srcDir = path.resolve(__dirname, '..', 'src');
+const distDir = path.resolve(__dirname, '..', 'dist');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', // translates CSS into CommonJS
+                    'sass-loader' // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(eot|woff|woff2|svg|ttf)([\?]?.*)$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+        new CleanWebpackPlugin(),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: '[id].css',
+        }),
+        new CopyPlugin([
+            {from: `${srcDir}/assets`, to: 'assets'},
+            {from: 'src/robots.txt', to: ''},
+            {from: 'src/manifest.json', to: ''},
+        ]),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, distDir),
+    },
+};
