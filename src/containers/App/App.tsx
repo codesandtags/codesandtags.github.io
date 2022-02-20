84c37a1fb67fb7b3c359cd7f6398d21d09919937
Edwin Torres <codesandtags@gmail.com>
2022-02-20 16:58:44 -0500

    feat: Added new structure with taildwind and vite

---
+++ src/containers/App/App.tsx
@@ -0,0 +1,14 @@
+import "react";
+import "./App.css";
+
+import Profile from "../../components/Profile/Profile";
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-black">
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const env = process.env.NODE_ENV || 'development'
-
-function buildConfig(env) {
-  return require('./config/webpack.' + env + '.js')
-}
-
-module.exports = buildConfig(env === 'production' ? 'prod' : 'dev')
