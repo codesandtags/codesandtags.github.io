fcab8439d8017ed8666572ea050a1fc507676c0a
Edwin Torres <codesandtags@gmail.com>
2022-02-20 21:34:13 -0500

    feat: Added particles

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -2,10 +2,12 @@ import "react";
 import "./App.css";
 
 import Profile from "../../components/Profile/Profile";
+import WraperParticles from "../../components/WraperParticles/WraperParticles";
 
 function App() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black">
+      <WraperParticles />
       <Profile />
     </div>
   );
