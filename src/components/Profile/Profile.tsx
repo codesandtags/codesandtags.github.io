684f3aec0a4b4ca56d6efe3c12c7f4e520b59b94
Edwin Torres <codesandtags@gmail.com>
2022-03-25 09:09:43 -0400

    feat: Updated texts

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import profile from "./profile.json";
 const Profile = () => {
   return (
     <>
-      <div className="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out bg-gray-100">
+      <div className="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3 bg-white shadow-lg  transform  duration-200 easy-in-out bg-gray-100">
         <div className="flex justify-center px-5  -mt-12">
           <img
             className="h-48 w-48 bg-white p-2 rounded-full"
@@ -17,8 +17,8 @@ const Profile = () => {
             <h2 className="text-gray-800 text-3xl font-bold">{profile.name}</h2>
             <p className="text-gray-400 mt-2">{profile.nickname}</p>
             <p className="mt-2 text-gray-600 text-center">
-            👋🏼 Hello, I'm a passionate <strong className="text-violet-600">Full-Stack Software Developer</strong>, 
-            I enjoy web development 💻, photography 📷, traveling 🌎, and sharing knowledge 📚.
+            👋🏼 Hello, I'm a <strong className="text-violet-600">Software Engineer</strong>. 
+            Every day I enjoy <span className="text-violet-600">[ 💻,  📷,  🌎, 📚]</span> and sharing knowledge.
             </p>
           </div>
           <hr className="mt-6" />
