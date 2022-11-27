688a7d9b873493ca23061c735dfaea2a60f7d238
codesandtags <codesandtags@edwins-MacBook-Pro.local>
2022-11-27 01:02:37 -0500

    feat: added meta tags for social network

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,23 +10,33 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-       <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-
-        <Script strategy="lazyOnload">
-            {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-                });
-            `}
-        </Script>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy="lazyOnload">
+        {`  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+      </Script>
 
       <Head>
-        <title>codesandtags | curiosidad intensa por la programación</title>
-        <meta name="description" content="Tutoriales de programación, experimentos de código y curiosidades del mundo del software" />
+        <title>Codes and Tags | Curiosidad intensa por la programación</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <meta name="description" content="Tutoriales de programación, experimentos de código y curiosidades del mundo del software. Curiosidad intensa por la programación." />
+        <meta name="theme-color" content="#449ed0" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@codesandtags" />
+        <meta name="twitter:creator" content="@codesandtags" />
+        <meta name="twitter:title" content="Codes and Tags" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Codes and Tags" />
+        <meta property="og:site_name" content="Codes and Tags" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale" content="es" />
       </Head>
 
       <main className={styles.main}>
