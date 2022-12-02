6829faf5b45f6378606d918459f536192acd5c3d
Edwin Torres <codesandtags@gmail.com>
2022-12-01 22:32:39 -0500

    feat: added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ThemeContext, themes } from '../lib/themes/ThemeContext'
-import BulbToggle from '../lib/components/bulb-toggle/BulbToggle';
-import Footer from '../lib/components/Footer';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeContext, themes } from "../lib/themes/ThemeContext";
+import BulbToggle from "../lib/components/bulb-toggle/BulbToggle";
+import Footer from "../lib/components/Footer";
 import styles from "../styles/Home.module.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <ThemeContext.Provider value={themes.dark}>
-      <nav className='flex flex-row flex-end justify-end'>
-        <BulbToggle />
-      </nav>
-      <main className={styles.main}>
-        <Component {...pageProps} />
-      </main>
-    <Footer />
-  </ThemeContext.Provider>
+   return (
+      <ThemeContext.Provider value={themes.dark}>
+         <Script
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+         />
+         <Script strategy="lazyOnload">
+            {`  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+         </Script>
+         <nav className="flex flex-row flex-end justify-end">
+            <BulbToggle />
+         </nav>
+         <main className={styles.main}>
+            <Component {...pageProps} />
+         </main>
+         <Footer />
+      </ThemeContext.Provider>
+   );
 }
