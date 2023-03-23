bc626f7fd80762f65c61c93222022e6af5fe5ea6
Edwin Torres <codesandtags@gmail.com>
2023-03-22 21:55:44 -0400

    feat: Updated typewritter

--- src/components/typewritter/Typewritter.tsx
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from "react";
-
-export default function Typewriter({ message }: { message: string }) {
-   const [text, setText] = useState("");
-
-   return <span className="typewriter-effect">{message}</span>;
-}
