43bd26672c6b7dc03f5a604b22c7eb38c90cc86b
Edwin Torres <codesandtags@gmail.com>
2023-07-27 23:29:35 -0400

    feat: updated styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,39 +9,24 @@ import Footer from "../src/components/Footer";
 sayHello();
 
 export default function Home() {
-   return (
-      <div className="container mx-auto">
-         <Head>
-            <title>
-               Codes and Tags | Curiosidad intensa por la programación
-            </title>
-            <link rel="icon" href="/favicon.ico" />
-            <MetaTags />
-         </Head>
+  return (
+    <div className="container mx-auto">
+      <Head>
+        <title>Codesand Tags | Curiosidad intensa por la programación</title>
+        <link rel="icon" href="/favicon.ico" />
+        <MetaTags />
+      </Head>
 
-         <div className="container flex items-center justify-center flex-col gap-8 mt-20 mb-20">
-            <div className="w-full flex items-center justify-center flex-col">
-               <Image
-                  src="/images/logo.png"
-                  width={150}
-                  height={150}
-                  alt={"logo"}
-               />
-               <Typewriter
-                  options={{
-                     strings: [
-                        "Bienvenido a Codes and Tags",
-                        "Curiosidad intensa por la programación",
-                        " :D ",
-                     ],
-                     autoStart: true,
-                     loop: false,
-                     deleteSpeed: 10,
-                  }}
-               />
-            </div>
-         </div>
-         <Footer />
+      <div className="container flex items-center justify-center flex-col gap-8 mt-20 mb-20">
+        <div className="w-full flex items-center justify-center flex-col">
+          <Image src="/images/logo.png" width={300} height={300} alt={"logo"} />
+          <h1 className="text-2xl">Codes and Tags</h1>
+          <h3 className="font-roboto mt-4">
+            Curiosidad intensa por la programación
+          </h3>
+        </div>
+        <Footer />
       </div>
-   );
+    </div>
+  );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,42 +1,35 @@
 import Infinite from "./infinite/Infinite";
 
 export default function Footer() {
-   return (
-      <footer className="container mx-auto flex flex-col items-center">
-         <div className="container">
-            <div className="w-full flex flex-col justify-center items-center content-center mx-auto py-4">
-               <ul className="flex flex-row gap-3 mt-4 mb-6">
-                  <a href="https://youtube.com/@codesandtags" target="_blank">
-                     <div className="icon icon-youtube"></div>
-                  </a>
-                  <a href="https://twitter.com/codesandtags" target="_blank">
-                     <div className="icon icon-twitter"></div>
-                  </a>
-                  <a
-                     href="https://www.instagram.com/codesandtags"
-                     target="_blank"
-                  >
-                     <div className="icon icon-instagram"></div>
-                  </a>
-                  <a
-                     href="https://ca.linkedin.com/company/codesandtags"
-                     target="_blank"
-                  >
-                     <div className="icon icon-linkedin"></div>
-                  </a>
-                  <a
-                     href="https://www.tiktok.com/@codesandtags"
-                     target="_blank"
-                  >
-                     <div className="icon icon-tiktok"></div>
-                  </a>
-                  <a href="https://github.com/codesandtags" target="_blank">
-                     <div className="icon icon-github"></div>
-                  </a>
-               </ul>
-            </div>
-         </div>
-         <Infinite />
-      </footer>
-   );
+  return (
+    <footer className="container mx-auto flex flex-col items-center">
+      <div className="container">
+        <div className="w-full flex flex-col justify-center items-center content-center mx-auto py-4">
+          <ul className="flex flex-row gap-3 mt-4 mb-6">
+            <a href="https://youtube.com/@codesandtags" target="_blank">
+              <div className="icon icon-youtube"></div>
+            </a>
+            <a href="https://twitter.com/codesandtags" target="_blank">
+              <div className="icon icon-twitter"></div>
+            </a>
+            <a href="https://www.instagram.com/codesandtags" target="_blank">
+              <div className="icon icon-instagram"></div>
+            </a>
+            <a
+              href="https://ca.linkedin.com/company/codesandtags"
+              target="_blank"
+            >
+              <div className="icon icon-linkedin"></div>
+            </a>
+            <a href="https://www.tiktok.com/@codesandtags" target="_blank">
+              <div className="icon icon-tiktok"></div>
+            </a>
+            <a href="https://github.com/codesandtags" target="_blank">
+              <div className="icon icon-github"></div>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
