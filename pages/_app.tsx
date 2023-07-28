857358b2dcad5601ef383b11589d879d3c17770e
Edwin Torres <codesandtags@gmail.com>
2023-07-28 15:17:25 -0400

    feat: removed parts and updated images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,25 +8,24 @@ import styles from "../styles/Home.module.css";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-   return (
-      <>
-         <Script
-            strategy="lazyOnload"
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-         />
-         <Script strategy="lazyOnload">
-            {`  window.dataLayer = window.dataLayer || [];
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload">
+        {`  window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
             page_path: window.location.pathname,
             });
         `}
-         </Script>
-         <nav className="container mx-auto flex flex-row flex-end justify-end"></nav>
-         <main className={styles.main}>
-            <Component {...pageProps} />
-         </main>
-      </>
-   );
+      </Script>
+      <main className={styles.main}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
