207d1ac597e993b9e6ac95baafcc4de9d4cc8d05
Edwin Torres <codesandtags@gmail.com>
2024-12-25 20:59:23 -0500

    fix: fixed lint

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,10 +1,8 @@
 'use client'
-import React, { useState } from 'react'
+import React from 'react'
 import Image from 'next/image'
 
 import { Github, Menu, X } from 'lucide-react'
-// import { useTheme } from './ThemeProvider'
-import { Badge } from '@/components/ui/badge'
 import { MenuItem } from '@/types/menu'
 import Link from 'next/link'
 import {
